605f5832797cd9fbddb23f6a2cd0b7a11f9ec196
Alexander Poe <shootnix@gmail.com>
2025-04-26 16:14:22 +0300

    add tests, fixes

--- lib/Contract/Declare.pm
+++ lib/Contract/Declare.pm
@@ -8,17 +8,16 @@ use Carp;
 
 our @EXPORT = qw(contract interface method returns);
 
-my $CHECK         = $ENV{I_CHECK_TYPES};
-my $KEEP_CONTRACT = $ENV{I_KEEP_CONTRACT};
-
-my $CURRENT_PKG;
+our $CURRENT_PKG;
 my %REGISTRY;
 
 sub contract {
-    ($CURRENT_PKG, my $block) = @_;
+    local $CURRENT_PKG;
+    my $block;
+    ($CURRENT_PKG, $block) = @_;
     $block->();
     _build_contract($CURRENT_PKG, $REGISTRY{$CURRENT_PKG});
-    delete $REGISTRY{$CURRENT_PKG} unless $KEEP_CONTRACT;
+    delete $REGISTRY{$CURRENT_PKG} unless $ENV{CONTRACT_DECLARE_KEEP_CONTRACT};
 }
 
 sub interface (&) { shift }
@@ -36,6 +35,21 @@ sub method {
         push @in, $p;
     }
 
+    for my $arg (@in) {
+        croak "Contract violation: input argument for method '$name' must be an object with 'compiled_check'"
+            unless blessed($arg) && $arg->can('compiled_check');
+    }
+
+    if ($out) {
+        croak "Contract violation: return type for method '$name' must be an arrayref"
+            unless ref($out) eq 'ARRAY';
+
+        for my $ret (@$out) {
+            croak "Contract violation: each return type for method '$name' must be an object with 'compiled_check'"
+                unless blessed($ret) && $ret->can('compiled_check');
+        }
+    }
+
     $REGISTRY{$CURRENT_PKG}{$name} = [ \@in, $out ];
 }
 
@@ -50,7 +64,7 @@ sub _build_contract {
 
         for my $method (keys %$contract) {
             my $code = $impl->can($method);
-            croak "impl does not implement $method" unless $code;
+            croak "Contract violation: Implementation does not provide method '$method' for interface '$pkg'" unless $code;
             $cache{$method} = $code;
         }
 
@@ -68,15 +82,11 @@ sub _build_contract {
         *{"${pkg}::$method"} = sub {
             my ($self, @args) = @_;
 
-            if ($CHECK) {
-                _validate(\@args, \@in_checks, "$pkg\::$method args");
-            }
+            _validate(\@args, \@in_checks, "$pkg\::$method args");
 
             my @res = $self->{_cache}{$method}->($self->{_impl}, @args);
 
-            if ($CHECK) {
-                _validate(\@res, \@out_checks, "$pkg\::$method return");
-            }
+            _validate(\@res, \@out_checks, "$pkg\::$method return");
 
             return wantarray ? @res : $res[0];
         };
@@ -93,12 +103,97 @@ sub _validate {
 
     return if @$checkers == 0;
 
-    croak "$label: wrong arg count" if @$values != @$checkers;
+    if (@$values != @$checkers) {
+        croak "Contract violation in $label: expected " . scalar(@$checkers) . " argument(s), got " . scalar(@$values);
+    }
 
     for (my $i = 0; $i < @$checkers; $i++) {
         next if $checkers->[$i]->($values->[$i]);
-        croak "$label: argument #$i failed check";
+        my $val = defined($values->[$i]) ? "'$values->[$i]'" : 'undef';
+        croak "Contract violation in $label: argument #$i ($val) failed type check";
     }
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Contract::Declare - Simple contract system for Perl interfaces
+
+=head1 SYNOPSIS
+
+    package MyInterface;
+    
+    use Contract::Declare;
+    use Standard::Types qw/Int Str/;
+    
+    contract 'MyInterface' => interface {
+        method add_number => (Str), returns(Int);
+        method get_name   => returns(Str);
+    };
+
+    package MyImpl;
+    
+    sub new { bless {}, shift }
+    
+    sub add_number { my ($self, $x) = @_; return $x + 1 }
+    
+    sub get_name { return "example" }
+
+    # Using the interface
+    my $impl = MyImpl->new;
+    my $obj = MyInterface->new($impl);
+
+    say $obj->add_number(41); # 42
+    say $obj->get_name;        # "example"
+
+=head1 DESCRIPTION
+
+C<Contract::Declare> allows you to define typed contracts (interfaces) in Perl,
+similar to lightweight design-by-contract.
+
+This module is intended for lightweight validation, especially useful during development
+or for critical internal components.
+
+=head1 FUNCTIONS
+
+=head2 contract $package => interface { ... }
+
+Defines a contract for the given package.
+
+- C<$package> is the full package name as a string (e.g., 'MyInterface').
+- C<interface { ... }> is a block where methods are declared.
+
+=head2 interface { ... }
+
+Marks the block containing method declarations.
+
+=head2 method $name => @input_types, returns(@output_types)
+
+Declares a method in the interface.
+
+- C<$name> is the method name (string).
+- C<@input_types> is a list of type objects (must implement C<compiled_check>).
+- C<returns(@output_types)> defines expected return types.
+
+Example:
+
+    method add => Int(), Int(), returns(Int());
+
+=head2 returns(@types)
+
+Specifies the expected return types for a method.
+
+=head1 AUTHOR
+
+Alexander Ponomarev <shootnix@gmail.com>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
